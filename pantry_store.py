d497af484f8c9b67d5ac9bf812a9f30718425cc3
Ron Harlev <harlev@gmail.com>
2020-09-11 17:00:18 -0700

    create and read working

--- pantry_store.py
+++ pantry_store.py
@@ -14,17 +14,21 @@ class PantryStore(AbstractJsonStorage):
         pantry_id = keys.PANTRY_ID
         basket_id = store_id or str(uuid.uuid4())
         self._set_store_id(basket_id)
-        return session.post(f"{self._get_api_root()}/pantry/{pantry_id}/basket/{basket_id})")
+        return self._request(session, "post", f"/pantry/{pantry_id}/basket/{basket_id}", json_data)
 
     def update(self, session, json_data=None, store_id=None):
         pass
 
     def read(self, session, json_data=None, store_id=None):
-        pass
+        pantry_id = keys.PANTRY_ID
+        basket_id = self.store_id
+        return self._request(session, "get", f"/pantry/{pantry_id}/basket/{basket_id}")
 
     def result_callback(self, method_name, result_content):
         print(f"{method_name} : {result_content}")
         if method_name == "create":
+            return result_content
+        if method_name == "read":
             return result_content
 
     def _get_api_root(self):
